e96ec1eb4e25071b6972020e0a377980ee9c6a31
Nick <kolya27.961@gmail.com>
2023-01-13 21:55:19 +0300

    Download book covers

--- tululu.py
+++ tululu.py
@@ -3,7 +3,7 @@ import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 import requests
-from urllib.parse import urljoin
+from urllib.parse import unquote, urljoin, urlsplit
 
 def check_for_redirect(checked_response):
     for response in checked_response.history:
@@ -21,9 +21,9 @@ def download_book(book_id):
     filename = f'{book_id}. {meta_info["title"]}'
     return download_txt(build_book_url(), filename)
 
-def get_book_meta_info(book_id):
-    url = f'https://tululu.org/b{book_id}'
 
+
+def get_book_meta_info(book_id):
     book_meta = {
                 'author': None,
                 'title': None
@@ -36,6 +36,7 @@ def get_book_meta_info(book_id):
     book_meta['author'] = author.strip()
 
     return book_meta
+
 
 def get_book_html(book_id):
     check_book_for_exist(book_id)
@@ -66,12 +67,35 @@ def check_book_for_exist(book_id):
     response.raise_for_status()
     check_for_redirect(response)
 
+
 def get_book_cover_link(book_id):
     soup = BeautifulSoup(get_book_html(book_id), 'lxml')
 
     img_rel_link = soup.find('div', class_='bookimage').find('img')['src']
     img_abs_link = urljoin('https://tululu.org/', img_rel_link)
     return img_abs_link
+
+
+def get_book_cover(book_id):
+    link = unquote(get_book_cover_link(book_id))
+    filename = os.path.basename(urlsplit(link).path)
+
+    return download_image(link, filename)
+
+
+def download_image(url, filename, folder='images/'):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    os.makedirs(folder, exist_ok=True)
+
+    path = f'{os.path.join(folder, sanitize_filename(filename))}'
+
+    with open(path, 'wb') as imgfile:
+        imgfile.write(response.content)
+    
+    return path
+
 
 def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
@@ -92,7 +116,7 @@ def main():
 
     for book_id in book_ids:
         try:
-            print(get_book_cover_link(book_id))
+            print(get_book_cover(book_id))
         except requests.HTTPError:
             continue
 
