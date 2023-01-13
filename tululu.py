aeee2db0030f1830b215f06a2e2a8f2aa6a5fa7f
Nick <kolya27.961@gmail.com>
2023-01-13 21:43:35 +0300

    Add get_book_cover_link function

--- tululu.py
+++ tululu.py
@@ -3,6 +3,7 @@ import os
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
 import requests
+from urllib.parse import urljoin
 
 def check_for_redirect(checked_response):
     for response in checked_response.history:
@@ -14,23 +15,11 @@ def download_book(book_id):
     url = 'https://tululu.org/txt.php'
     
 
-    params = {
-                'id': book_id
-            }
-    
-    prereq = requests.models.PreparedRequest()
-    prereq.prepare_url(url, params)
-
-    full_url = prereq.url
-
-    # Checking is file exists
-    response = requests.get(full_url)
-    response.raise_for_status()
-    check_for_redirect(response)
+    check_book_for_exist(book_id)
     
     meta_info = get_book_meta_info(book_id)
     filename = f'{book_id}. {meta_info["title"]}'
-    return download_txt(full_url, filename)
+    return download_txt(build_book_url(), filename)
 
 def get_book_meta_info(book_id):
     url = f'https://tululu.org/b{book_id}'
@@ -39,10 +28,8 @@ def get_book_meta_info(book_id):
                 'author': None,
                 'title': None
             }
-    response = requests.get(url)
-    response.raise_for_status()
 
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(get_book_html(book_id), 'lxml')
 
     title, author = soup.find('td', class_='ow_px_td').find('h1').text.split('::')
     book_meta['title'] = title.strip()
@@ -50,6 +37,41 @@ def get_book_meta_info(book_id):
 
     return book_meta
 
+def get_book_html(book_id):
+    check_book_for_exist(book_id)
+    url = f'https://tululu.org/b{book_id}'
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    return response.text
+
+
+def build_book_url(book_id):
+    url = 'https://tululu.org/txt.php'
+    
+
+    params = {
+                'id': book_id
+            }
+    
+    prereq = requests.models.PreparedRequest()
+    prereq.prepare_url(url, params)
+
+    return prereq.url
+
+
+def check_book_for_exist(book_id):
+    response = requests.get(build_book_url(book_id))
+    response.raise_for_status()
+    check_for_redirect(response)
+
+def get_book_cover_link(book_id):
+    soup = BeautifulSoup(get_book_html(book_id), 'lxml')
+
+    img_rel_link = soup.find('div', class_='bookimage').find('img')['src']
+    img_abs_link = urljoin('https://tululu.org/', img_rel_link)
+    return img_abs_link
 
 def download_txt(url, filename, folder='books/'):
     response = requests.get(url)
@@ -70,7 +92,7 @@ def main():
 
     for book_id in book_ids:
         try:
-            download_book(book_id)
+            print(get_book_cover_link(book_id))
         except requests.HTTPError:
             continue
 
