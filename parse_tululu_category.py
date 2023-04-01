c1d415e50ca4f51c5478714cd763410788e7f3a2
Nick <kolya27.961@gmail.com>
2023-04-01 22:38:35 +0300

    Parsed all links on category page

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -7,8 +7,12 @@ from tululu import get_html
 def main():
     html = get_html('https://tululu.org/l55/')
     soup = BeautifulSoup(html, 'lxml')
-    rel_link = soup.find('table', class_='d_book').find('a')['href']
-    print(urljoin('https://tululu.org', rel_link))
+    rel_links = [i.find('a')['href']
+                 for i in soup.find_all('table', class_='d_book')]
+    abs_links = [urljoin('https://tululu.org', rel_link)
+                 for rel_link in rel_links]
+    for link in abs_links:
+        print(link)
 
 
 if __name__ == '__main__':
