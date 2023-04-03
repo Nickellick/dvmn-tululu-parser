ad80bc6a3fc261c66eace49c4942e2ed7c0c7b3f
Nick <kolya27.961@gmail.com>
2023-04-03 20:07:53 +0300

    Downloaded first 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -5,14 +5,19 @@ from tululu import get_html
 
 
 def main():
-    html = get_html('https://tululu.org/l55/')
-    soup = BeautifulSoup(html, 'lxml')
-    rel_links = [i.find('a')['href']
-                 for i in soup.find_all('table', class_='d_book')]
-    abs_links = [urljoin('https://tululu.org', rel_link)
-                 for rel_link in rel_links]
-    for link in abs_links:
-        print(link)
+    category_url = 'https://tululu.org/l55/'
+    for page_num in range(1, 11):
+        if page_num == 1:
+            html = get_html(category_url)
+        else:
+            html = get_html(urljoin(category_url, f'{page_num}'))
+        soup = BeautifulSoup(html, 'lxml')
+        rel_links = [i.find('a')['href']
+                     for i in soup.find_all('table', class_='d_book')]
+        abs_links = [urljoin('https://tululu.org', rel_link)
+                     for rel_link in rel_links]
+        for link in abs_links:
+            print(link)
 
 
 if __name__ == '__main__':
