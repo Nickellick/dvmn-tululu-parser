5c883b94715eff0552fd0646d10cf1c0a75fa321
Nick <kolya27.961@gmail.com>
2023-05-19 23:29:58 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-
+from livereload import Server
 
 def load_template(path='template.html'):
     env = Environment(
@@ -20,17 +20,24 @@ def localize_book_cover(books, path='result/covers'):
         book['alt'] = f'{book["author"]} {book["title"]}'
 
 
+def on_reload(books):
+    template = load_template()
+    rendered_page = template.render(
+        books=books
+    )
+    with open('index.html', 'w', encoding='utf8') as file:
+        file.write(rendered_page)
+
+
 def main():
     books = {}
     with open('result/books.json', 'r') as file:
         books = json.load(file)
     localize_book_cover(books)
-    template = load_template()
-    rendered_page = template.render(
-        books=books
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    server = Server()
+    on_reload(books)
+    server.watch('template.html', lambda: on_reload(books))
+    server.serve(root='.')
 
 
 if __name__ == '__main__':
