ccb39173ee85db46cd41100cdd74c4711cc73f90
Nick <kolya27.961@gmail.com>
2023-05-19 23:54:10 +0300

    Add links to books txt files

--- render_website.py
+++ render_website.py
@@ -4,6 +4,7 @@ import os
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
+from urllib.parse import quote
 
 
 def load_template(path='template.html'):
@@ -18,8 +19,16 @@ def load_template(path='template.html'):
 def localize_book_cover(books, path='result/covers'):
     for book in books:
         ext = book['cover'].split('.')[-1]
-        book['cover'] = os.path.join(path, f'{book["id"]}.{ext}')
+        book['cover'] = quote(os.path.join(path, f'{book["id"]}.{ext}'))
         book['alt'] = f'{book["author"]} {book["title"]}'
+
+
+def add_text_link(books, path='result/books/'):
+    for book in books:
+        book['text'] = quote(os.path.join(
+            path,
+            f'{book["id"]}. {book["title"]}.txt'
+        ))
 
 
 def on_reload(books):
@@ -36,10 +45,12 @@ def main():
     with open('result/books.json', 'r') as file:
         books = json.load(file)
     localize_book_cover(books)
+    add_text_link(books)
     server = Server()
     on_reload(books)
     server.watch('template.html', lambda: on_reload(books))
     server.serve(root='.')
+
 
 
 if __name__ == '__main__':
