c581b5c8ad27a9871f795d1216276bea0422b702
Nick <kolya27.961@gmail.com>
2023-05-20 01:01:30 +0300

    Split books by pages

--- render_website.py
+++ render_website.py
@@ -16,14 +16,14 @@ def load_template(path='template.html'):
     return env.get_template(path)
 
 
-def localize_book_cover(books, path='result/covers'):
+def localize_book_cover(books, path='../result/covers'):
     for book in books:
         ext = book['cover'].split('.')[-1]
         book['cover'] = quote(os.path.join(path, f'{book["id"]}.{ext}'))
         book['alt'] = f'{book["author"]} {book["title"]}'
 
 
-def add_text_link(books, path='result/books/'):
+def add_text_link(books, path='../result/books/'):
     for book in books:
         book['text'] = quote(os.path.join(
             path,
@@ -33,11 +33,15 @@ def add_text_link(books, path='result/books/'):
 
 def on_reload(books):
     template = load_template()
-    rendered_page = template.render(
-        book_chunks=chunked(books, 2)
-    )
-    with open('index.html', 'w', encoding='utf8') as file:
-        file.write(rendered_page)
+    books_paged = chunked(books, 20)
+    for i, books_page in enumerate(books_paged):
+        rendered_page = template.render(
+            book_chunks=chunked(books_page, 2)
+        )
+        os.makedirs('pages', exist_ok=True)
+        with open(os.path.join('pages', f'index{i+1}.html'), 'w',
+                  encoding='utf8') as file:
+            file.write(rendered_page)
 
 
 def main():
@@ -49,7 +53,7 @@ def main():
     server = Server()
     on_reload(books)
     server.watch('template.html', lambda: on_reload(books))
-    server.serve(root='.')
+    server.serve(root='.', default_filename='pages/index1.html')
 
 
 
