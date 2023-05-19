e942d03bc16dae5c6eee07d7453fb122b4e969f4
Nick <kolya27.961@gmail.com>
2023-05-19 23:19:38 +0300

    Split to separate functions

--- render_website.py
+++ render_website.py
@@ -1,29 +1,37 @@
 import json
-import pathlib
+import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
 
-template = env.get_template('template.html')
+def load_template(path='template.html'):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-books = {}
-
-with open('result/books.json', 'r') as file:
-    books = json.load(file)
-
-for book in books:
-    ext = book['cover'].split('.')[-1]
-    book['cover'] = f'result/covers/{book["id"]}.{ext}'
-    book['alt'] = f'{book["author"]} {book["title"]}'
+    return env.get_template(path)
 
 
-rendered_page = template.render(
-    books=books
-)
+def localize_book_cover(books, path='result/covers'):
+    for book in books:
+        ext = book['cover'].split('.')[-1]
+        book['cover'] = os.path.join(path, f'{book["id"]}.{ext}')
+        book['alt'] = f'{book["author"]} {book["title"]}'
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+
+def main():
+    books = {}
+    with open('result/books.json', 'r') as file:
+        books = json.load(file)
+    localize_book_cover(books)
+    template = load_template()
+    rendered_page = template.render(
+        books=books
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+
+if __name__ == '__main__':
+    main()        
