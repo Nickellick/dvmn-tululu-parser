3e1e1a9487a092917a7a3423c60c89f7a29833ec
Nick <kolya27.961@gmail.com>
2023-05-20 19:59:05 +0300

    Working pagination

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -32,11 +33,26 @@ def add_text_link(books, path='../result/books/'):
 
 
 def on_reload(books):
+    books_per_page = 20
     template = load_template()
-    books_paged = chunked(books, 20)
+    books_paged = chunked(books, books_per_page)
+    total_pages = math.ceil(len(books) / books_per_page)
     for i, books_page in enumerate(books_paged):
+        if i == 0:
+            pages = [1, 2, 3]
+            paginator_selector = 0
+        elif i == total_pages - 1:
+            pages = [total_pages - 2, total_pages - 1, total_pages]
+            paginator_selector = 2
+        else:
+            pages = [i, i+1, i+2]
+            paginator_selector = 3
+        rel_page_links = [f'/pages/index{page}.html' for page in pages]
         rendered_page = template.render(
-            book_chunks=chunked(books_page, 2)
+            book_chunks=chunked(books_page, 2), 
+            rel_page_links=rel_page_links,
+            paginator_selector=paginator_selector,
+            pages=pages
         )
         os.makedirs('pages', exist_ok=True)
         with open(os.path.join('pages', f'index{i+1}.html'), 'w',
